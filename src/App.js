f2e9d0959b82c1f96ced92052bdc0385a016194d
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-09-08 02:21:27 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-          <Navbar/>
-   
+          <Navbar></Navbar>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,33 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
-import {
-    MdLanguage,
-    MdOutlineDarkMode,
-    MdOutlineFullscreenExit,
-    MdOutlineNotificationsNone,
-    MdOutlineChatBubbleOutline,
-    MdList
-} from "react-icons/md"
+import { BsDiscord } from "react-icons/bs"
+import { RiQuestionAnswerFill } from "react-icons/ri"
+
 import "./Navbar.scss"
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
     let leftItems = [
-        { icon: <MdLanguage />, text: "English" },
-        { icon: <MdOutlineDarkMode /> },
-        { icon: <MdOutlineNotificationsNone />, notification: 1 },
-        { icon: <MdOutlineChatBubbleOutline />, notification: 1 },
-        { icon: <MdList /> }
+        { icon: <BsDiscord />, text: "Discord", url: "https://discord.gg/j4j" },
+        { icon: <RiQuestionAnswerFill />, text: "Any Question?", text2: "Contact Arvelos", url: "" },
     ]
 
     const leftMenu = leftItems.map(item => {
         let id = uuidv4();
         return (
             <div className="item" key={id.toString()}>
-                {item.icon}
-                {item.text && item.text}
-                {item.notification && (
-                    <div className="notification">
-                        {item.notification}
-                    </div>
-                )}
+                <span className='icon'>{item.icon}</span>
+                <div>
+                    <div className='text'>{item.text && item.text}</div>
+                    <div className='text'>{item.text2 && item.text2}</div>
+                </div>
+
             </div>
         )
     })
 
     let middleItems = [
-        { text: "Metamask" },
         { text: "Home" },
         { text: "Collections" },
     ]
@@ -54,7 +45,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="wrapper">
                 <div className="logo">
-                    <img src="https://cdn.discordapp.com/attachments/616971277059620901/1017177715419389982/a_ca5ecd9ae0b50af5a9213582b0428b9e.gif" alt="logo" className="logo" />
+                    <img src="https://cdn.discordapp.com/attachments/616971277059620901/1017177715419389982/a_ca5ecd9ae0b50af5a9213582b0428b9e.gif" alt="logo"/>
                     <div className='animate-charcter'>Gif Share</div>
                 </div>
                 <div className="block-1">
@@ -62,9 +53,6 @@ const Navbar = () => {
                 </div>
                 <div className="block-2">
                     {leftMenu}
-                    <div className="item">
-                        <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" alt="Profil" />
-                    </div>
                 </div>
             </div>
         </div>
